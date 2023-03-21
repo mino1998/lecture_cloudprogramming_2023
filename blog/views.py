6c4d41b6df73e31e7ba18ae63bc804ef2d239faa
Lee Min Ho <mhlee5894@gmail.com>
2023-03-21 16:56:25 +0900

    blog view update

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from . models import Post
 
-# Create your views here.
+def index(request):
+    posts=Post.objects.all().order_by('-pk')  #created_at 넣으면 생성시간 순서대로겠지, -를 붙이면 내림차순이다.
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts':posts,
+        }
+    )
