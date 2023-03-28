553b02e60ef10e32ca01a285e2776095143b8871
Lee Min Ho <mhlee5894@gmail.com>
2023-03-28 15:35:42 +0900

    make listpost and detailpost

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render
 from . models import Post
+from django.views.generic import ListView, DetailView
 
-def index(request):
-    posts=Post.objects.all().order_by('-pk')  #created_at 넣으면 생성시간 순서대로겠지, -를 붙이면 내림차순이다.
+class PostList(ListView):
+    model=Post
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts':posts,
-        }
-    )
+class PostDetail(DetailView):
+    model=Post
+# def index(request):
+#     posts=Post.objects.all().order_by('-pk')  #created_at 넣으면 생성시간 순서대로겠지, -를 붙이면 내림차순이다.
+#
+#     return render(
+#         request,
+#         'blog/post_list.html',
+#         {
+#             'posts':posts,
+#         }
+#     )
 
-def single_post_page(request, pk):
-    post=Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post':post,
-        }
-    )
+# def single_post_page(request, pk):
+#     post=Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'post':post,
+#         }
+#     )
