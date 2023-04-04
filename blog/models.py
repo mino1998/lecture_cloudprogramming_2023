a1282df9b43fc80a293e7de06e816d76ca7980dc
Lee Min Ho <mhlee5894@gmail.com>
2023-04-04 16:16:12 +0900

    add category

--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,15 @@ from django.contrib.auth.models import User
 import os
 from django.db import models
 
+class Category(models.Model):
+    name=models.CharField(max_length=50, unique=True) #유일한 값을 가지게 하는 유니크
+    slug=models.SlugField(max_length=200,unique=True, allow_unicode=True) #slugfield는 한글을 허용하지 않으나, allow값을 true로 하면 사용 가능하다.
+    ##slug란 무엇인가? pk or 글의 일부가 url에 일부에 들어갈 수 있다.
+    def __str__(self):
+        return self.name
 
-
+    class Meta:
+        verbose_name_plural='Categories'
 
 class Post(models.Model):
     title=models.CharField(max_length=30)
@@ -17,6 +24,9 @@ class Post(models.Model):
     updated_at=models.DateTimeField(auto_now=True)
     author=models.ForeignKey(User, null=True, on_delete=models.SET_NULL) #user class가 없으니까 쟝고에 user라는 모델 불러올거에요~
     #cascade는 계단식이라 사라지면 전부 사라지게 만든다 이말이에요
+
+    category=models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL) #기본 카테고리로 가던가, 카테고리 없음으로 가려면 set_null, 왜냐면 null 이 true니까
+    
 
     def __str__(self):
         return f'[{self.pk}] {self.title}::{self.author}' #: 앞에 키값이 나오고
