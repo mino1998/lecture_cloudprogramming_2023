15f1e525c55996aaf7381d82c11b6ee22800d12c
Lee Min Ho <mhlee5894@gmail.com>
2023-04-04 15:30:01 +0900

    add author

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 import os
 from django.db import models
+
+
+
 
 class Post(models.Model):
     title=models.CharField(max_length=30)
@@ -12,10 +15,11 @@ class Post(models.Model):
     file_upload=models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
-    #author : 추후 작성 예정
+    author=models.ForeignKey(User, null=True, on_delete=models.SET_NULL) #user class가 없으니까 쟝고에 user라는 모델 불러올거에요~
+    #cascade는 계단식이라 사라지면 전부 사라지게 만든다 이말이에요
 
     def __str__(self):
-        return f'[{self.pk}] {self.title}' #: 앞에 키값이 나오고
+        return f'[{self.pk}] {self.title}::{self.author}' #: 앞에 키값이 나오고
         #return self.title 타이틀 값만
 
     def get_absolute_url(self):
